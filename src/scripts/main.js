2e93560634d67dca2485ace8a91da8987eda00a3
Yellow Moon <yellowmoonastronaut@gmail.com>
2022-09-29 15:43:46 -0300

    update functions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,48 +2,140 @@
 const Main = {
     start: function () {
         this.casheSelectors()
+        this.createDefaltTasks()
+        this.showTasks()
         this.addEvents()
     },
 
 
 
     casheSelectors: function () {
-        const tasksAdd = document.querySelector('#tasksAdd')
-        const tasksContent = document.querySelector('#tasksContent')
-        // const tasksRemove = document.querySelectorAll('#tasksRemove')
+        this.$tasksContent = document.querySelector('#tasksContent')
+        this.$deleteAll = document.querySelector('#deleteAll')
     },
     
+
+    createDefaltTasks: function () {
+        this.taskListDefault = [
+            {
+                id: 'defaultTask',
+                content: `
+                    <li id="defaultTask" class="task">
+                        <div  class="check-task">
+                            <div>
+                                <i class="ph-circle" title="Marcar como concluída"></i>
+                            </div>
+                            <p>Descrição da tarefa</p>
+                        </div>
+                        <i class="ph-trash tasksRemove" title="Excuir"></i>
+                    </li>
+                `
+            },
+        ]
+    },
+
+
+    showTasks: function () {
+        let tasksList = ''
+        this.taskListDefault.map((tasks) => {
+            tasksList += tasks.content
+        })
+       this.$tasksContent.innerHTML = tasksList
+    },
+
+
     addEvents: function () {
-        tasksAdd.addEventListener('keydown', (event) => {
-                if (event.key === 'Enter'){this.myEvents.addTask()}
-            })
+        const $tasksAdd = document.querySelector('#tasksAdd')
+        $tasksAdd.onkeydown = (e) => { this.addTask(e) }
+
+        const $tasksRemove = document.querySelectorAll('.tasksRemove')
+        $tasksRemove.forEach(task => {
+            task.onclick = (e) => { this.removeTask(e) }
+        })
+
+        const $checkTask = document.querySelectorAll('.check-task')
+        $checkTask.forEach(task => {
+            task.onclick = (e) => {
+                this.doneTask(e, task)
+            }
+        })
+
+        this.$deleteAll.onclick = () => {this.deleteAllTasks()}
     },
 
 
 
-    myEvents: {
-        addTask: function () {
-            const task = document.querySelector('#tasksAdd')
-            const taskSlug = (task.value
-                .toLowerCase()
-                .trim()
-                .replace(/[^\w\s-]/g, '')
-                .replace(/[\s_-]+/g, '-')
-                .replace(/^-+|-+$/g, '')
-            )
-            if (task.value !== "") {
-                tasksContent.innerHTML += `
-                    <div id="${taskSlug}" class="task"><div><i title="Marcar como concluída" class="ph-circle"></i><p>${task.value}</p></div><i id="tasksRemove" onclick="Main.myEvents.removeTask('${taskSlug}')" class="ph-trash"></i></div>
-                `
-                task.value = ''
+    addTask: function (e) {
+        if (e.key === 'Enter'){
+            const $task = document.querySelector('#tasksAdd')
+            if ($task.value !== "") {
+                const dateSlug = new Date().getMilliseconds()
+                const taskSlug = (
+                    $task.value
+                    .toLowerCase()
+                    .trim()
+                    .replace(/[^\w\s-]/g, '')
+                    .replace(/[\s_-]+/g, '-')
+                    .replace(/^-+|-+$/g, '')
+                )
+                this.taskListDefault.push({
+                    id: `${taskSlug}${dateSlug}`,
+                    content: `
+                        <li id="${taskSlug}${dateSlug}" class="task">
+                            <div class="check-task">
+                                <div>
+                                    <i class="ph-circle" title="Marcar como concluída"></i>
+                                </div>
+                                <p>${$task.value}</p>
+                            </div>
+                            <i class="ph-trash tasksRemove" title="Excuir"></i>
+                        </li>
+                    `
+                })
+                $task.value = ''
+                this.showTasks()
+                this.addEvents()
             }
-        },
-        removeTask: function (id) {
-            
         }
     },
 
+    
+    removeTask: function (e) {
+
+        console.log(e.target.parentElement.id)
+
+
+        this.taskListDefault.map((i, index) => {
+            if(i.id === e.target.parentElement.id) {
+                this.taskListDefault.splice(index, 1)
+            }
+        })
+        this.showTasks()
+        this.addEvents()
+    },
+
+
+    doneTask: function (e, task) {
+        task.classList.toggle('task-done')
+
+        console.log(this)
+
+        this.taskListDefault.map((i) => {
+            if(i.id === e.target.parentElement.parentElement.parentElement.id) {
+                i.content = e.target.parentElement.parentElement.parentElement.outerHTML
+            }
+        })
+    },
+
+
+    deleteAllTasks: function () {
+        this.taskListDefault = []
+        this.showTasks()
+        this.addEvents()
+    },
 }
+
+
 
 // DOM Loaded
 document.addEventListener('DOMContentLoaded', () => {
